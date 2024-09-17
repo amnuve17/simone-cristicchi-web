0845c3a004a9a3065fd9e8f16742558dcec3fe7b
Nuve <156921808+amnuve17@users.noreply.github.com>
2024-09-17 20:45:59 +0200

    Update CartProvider.jsx

--- src/providers/CartProvider.jsx
+++ src/providers/CartProvider.jsx
@@ -1,36 +1,38 @@
-import { createContext, useContext, useEffect, useState } from "react"
+import { createContext, useContext, useEffect, useState } from "react";
 
 const CartContext = createContext({
     cart: [],
-})
+});
 
-
-export const useCart = () => useContext(CartContext)
-
-const cartCookie = localStorage.getItem('cart');
-console.log(cartCookie);
+export const useCart = () => useContext(CartContext);
 
 const CartProvider = ({ children }) => {
     const [cart, setCart] = useState([]);
-    console.log(cart);
-    
-    useEffect(() => {
-        localStorage.setItem('cart', JSON.stringify(cart));
-    }, [cart]);
 
+    // Questo effetto si esegue solo lato client
     useEffect(() => {
-        const cartString = localStorage.getItem('cart');
-        if (cartString) {
-            const storedCart = JSON.parse(cartString); // Deserialize the stored cart string to an object
-            setCart(storedCart);
+        // Controlla se siamo lato client
+        if (typeof window !== "undefined") {
+            const cartString = localStorage.getItem('cart');
+            if (cartString) {
+                const storedCart = JSON.parse(cartString); // Deserialize the stored cart string to an object
+                setCart(storedCart);
+            }
         }
-    }, []);
+    }, []); // Esegue solo al montaggio
+
+    // Effettua il salvataggio del carrello nel localStorage quando cambia
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            localStorage.setItem('cart', JSON.stringify(cart));
+        }
+    }, [cart]); // Esegue ogni volta che `cart` cambia
 
     return (
         <CartContext.Provider value={{ cart, setCart }}>
             {children}
         </CartContext.Provider>
-    )
-}
+    );
+};
 
-export default CartProvider
+export default CartProvider;
